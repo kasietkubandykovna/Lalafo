32aeec03dcbf2ef3743657b695252397dc15374a
Omurzak <soorbaevo@gmail.com>
2024-07-09 01:09:40 +0600

    changes main

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,107 @@ import models.Announcement;
 import models.Favorite;
 import models.User;
 import service.AnnouncementService;
+import service.FavoriteService;
 import service.serviceImpl.AnnouncementServiceImpl;
 import service.serviceImpl.FavoriteServiceImpl;
 import service.serviceImpl.UserServiceImpl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl user =new UserServiceImpl();
-        FavoriteServiceImpl favorite =new FavoriteServiceImpl();
-        user.addUser(new User(1L,"Kasiet","Kasiet@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()));
-        user.addUser(new User(2L,"Kurmanbek","Kurmanbek@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()));
-        user.addUser(new User(3L,"Nurkyz","Nurkyz@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()));
-        user.addUser(new User(4L,"Omurzak baike","Omurzak.baike@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()));
-        System.out.println(user.getAllUsers());
-//        user.updateUser(1L, new User("Asyl","Asyl@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()));
-        System.out.println(user.deleteUser(4L));
-
-        AnnouncementService announcement =new AnnouncementServiceImpl();
-        System.out.println(announcement.addAnnouncement(1L, new Announcement(1L, "iphone14", "sostayanie zynk baterii 93%", 60000, "Kasiet")));
-        System.out.println(announcement.addAnnouncement(1L, new Announcement(2L, "iphone12", "sostayanie zynk baterii 93%", 30000, "Kasiet")));
-        System.out.println(announcement.addAnnouncement(1L, new Announcement(3L, "iphone13", "sostayanie zynk baterii 93%", 45000, "Kasiet")));
-        System.out.println(announcement.getAllAnnouncement(1L));
-        System.out.println(announcement.getAnnouncementById(2L));
-        announcement.updateAnnouncement(2L,new Announcement("iphone15","new",13000,"Kasiet"));
-        System.out.println(announcement.getAnnouncementById(2L));
-        System.out.println(announcement.deleteAnnouncement(3L));
-        System.out.println(announcement.getAllAnnouncement(1L));
-
-        System.out.println(favorite.addToFavorite(1L,2L,2L));
-        System.out.println(user.getAllUsers());
-        System.out.println(favorite.getAllFavoriteByUserId(2L));
-        System.out.println(user.getUserById(2L));
-        System.out.println(favorite.getFavoriteByID(2L, 1L));
-//        System.out.println(favorite.deleteFavoritebyId(3L, 1L));
-        System.out.println(user.getUserById(2L));
+        AnnouncementService announcement = new AnnouncementServiceImpl();
+        FavoriteService favorite = new FavoriteServiceImpl();
 
 
+        System.out.println("""
+                1. save new user
+                2. get all users
+                3. get all user by id
+                4. update user
+                5. delete user
+                6. save announcement
+                7. get all announcements
+                8. get all by id
+                9. update announcement
+                10. delete announcement
+                11. save favorite
+                12. get favorite by id
+                13. get all favorite by user id
+                14. delete favorite by id""");
+        List<User> users = Arrays.asList(
+                new User(1L,"Kasiet","Kasiet@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()),
+        new User(2L,"Kurmanbek","Kurmanbek@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()),
+        new User(3L,"Nurkyz","Nurkyz@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()),
+        new User(4L,"Omurzak baike","Omurzak.baike@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>())
+        );
+
+        List<Announcement> announcements = Arrays.asList(
+                new Announcement(1L, "iphone14", "sostayanie zynk baterii 93%", 60000, "Kasiet"),
+                new Announcement(2L, "iphone12", "sostayanie zynk baterii 93%", 30000, "Kasiet"),
+                new Announcement(3L, "iphone13", "sostayanie zynk baterii 93%", 45000, "Kasiet"));
 
 
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+
+//                    String imput = sc.nextLine();
+//                int number = Integer.parseInt(imput);
+            int number = sc.nextInt();
+                switch (number) {
+                    case 1-> {
+                        for (User user1: users){
+                            user.addUser(user1);
+                        }
+                    }
+                    case 2-> System.out.println(user.getAllUsers());
+                    case 3-> System.out.println(user.getUserById(1L));
+                    case 4-> user.updateUser(2L,new User("Asyl","Asyl@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()));
+                    case 5-> System.out.println(user.deleteUser(4L));
+//                    case 6-> System.out.println(announcement.addAnnouncement(1L,announcements));
+                    case 7-> System.out.println(announcement.getAllAnnouncement(1L));
+                    case 8-> System.out.println(announcement.getAnnouncementById(2L));
+                    case 9-> announcement.updateAnnouncement(2L,new Announcement("iphone15","new",13000,"Kasiet"));
+                    case 10-> System.out.println(announcement.deleteAnnouncement(3L));
+                    case 11-> System.out.println(favorite.addToFavorite(1L,2L,2L));
+                    case 12-> System.out.println(favorite.getFavoriteByID(2L, 1L));
+                    case 13-> System.out.println(favorite.getAllFavoriteByUserId(1L));
+                    case 14-> System.out.println(favorite.deleteFavoritebyId(2L, 1L));
+                }
+
+
+
+//        FavoriteServiceImpl favorite =new FavoriteServiceImpl();
+
+//        System.out.println(user.getAllUsers());
+////        user.updateUser(1L, new User("Asyl","Asyl@gmail.com",12345, Role.USER,new ArrayList<>(),new ArrayList<>()));
+//        System.out.println(user.deleteUser(4L));
+//
+//        AnnouncementService announcement =new AnnouncementServiceImpl();
+
+//        System.out.println(announcement.getAllAnnouncement(1L));
+//        System.out.println(announcement.getAnnouncementById(2L));
+//        announcement.updateAnnouncement(2L,new Announcement("iphone15","new",13000,"Kasiet"));
+//        System.out.println(announcement.getAnnouncementById(2L));
+//        System.out.println(announcement.deleteAnnouncement(3L));
+//        System.out.println(announcement.getAllAnnouncement(1L));
+//
+//        System.out.println(favorite.addToFavorite(1L,2L,2L));
+//        System.out.println(user.getAllUsers());
+//        System.out.println(favorite.getAllFavoriteByUserId(2L));
+//        System.out.println(user.getUserById(2L));
+//        System.out.println(favorite.getFavoriteByID(2L, 1L));
+////        System.out.println(favorite.deleteFavoritebyId(3L, 1L));
+//        System.out.println(user.getUserById(2L));
+
+
+
+
+    }
     }
 }
--- src/dao/AnnouncementDao.java
+++ src/dao/AnnouncementDao.java
@@ -5,7 +5,7 @@ import models.Announcement;
 import java.util.List;
 
 public interface AnnouncementDao {
-    String addAnnouncement(Long userId, Announcement announcement);
+    String addAnnouncement(Long userId,  Announcement announcement);
     List<Announcement> getAllAnnouncement(Long userId);
     Announcement getAnnouncementById(Long id);
     void updateAnnouncement(Long id,Announcement newAnnouncement);
